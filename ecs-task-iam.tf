e5de3f19cde7dfae57484912156d4ee2a100cab3
vrjfsyi <34932735+vrjfsyi@users.noreply.github.com>
2019-01-17 07:05:56 +0900

    create ecs task iam role

---
+++ ecs-task-iam.tf
@@ -0,0 +1,34 @@
+
+# Create IAM Role and Policy for ECS Task
+
+# Create IAM Assume Role Policy Document
+data "aws_iam_policy_document" "ecs-task-assume-role-iam-policy-document" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "ecs-tasks.amazonaws.com",
+      ]
+    }
+  }
+}
+
+## Create IAM role for ECS Task
+resource "aws_iam_role" "ecs-task-iam-role" {
+  name               = "${var.APPLICATION}-${var.ENVIRONMENT}-ecs-task-iam-role"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs-task-assume-role-iam-policy-document.json}"
+  path               = "/${var.APPLICATION}/${var.ENVIRONMENT}/"
+  description        = "iam role for ecs task"
+
+  tags = {
+    Name        = "${var.APPLICATION}-${var.ENVIRONMENT}-ecs-task-iam-role"
+    Application = "${var.APPLICATION}"
+    Environment = "${var.ENVIRONMENT}"
+    Terraform   = "true"
+  }
+}
